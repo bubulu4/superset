737c957bacd44303be2cf4650208514e5e14ad9d
Maxime Beauchemin <maximebeauchemin@gmail.com>
2025-01-05 10:28:42 -0800

    switch & span

--- superset-frontend/src/components/Tabs/Tabs.tsx
+++ superset-frontend/src/components/Tabs/Tabs.tsx
@@ -17,7 +17,8 @@
  * under the License.
  */
 import { css, styled } from '@superset-ui/core';
-import AntdTabs, { TabsProps as AntdTabsProps } from 'antd/lib/tabs';
+import { Tabs as AntdTabs } from 'antd-v5';
+import type { TabsProps } from 'antd-v5';
 import Icons from 'src/components/Icons';
 
 export interface TabsProps extends AntdTabsProps {
